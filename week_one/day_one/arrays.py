699ce84b7653f2d6b8d4972ccb5a720cbcfe9d2f
kemoeverlyne <kemoeverlyne@gmail.com>
2023-09-10 14:13:03 +0300

    arrays

---
+++ week_one/day_one/arrays.py
@@ -0,0 +1,129 @@
+# declaring and initializing arrays in python
+my_list = []
+
+#initializing a list with values
+my_list = [1,2,3,4,5]
+
+#initializing a list with mixed data types
+mix_list = [1, "Hello", True, 3.14]
+
+
+
+#=================================================================
+
+#           ACCESSING ELEMENTS
+
+#==================================================================
+
+# Python uses "0" based indexing which means the first element of the
+# list has an index of "0"
+
+my_list = [10, 20, 30, 40, 50]
+
+'''
+Accessing the first element of the list
+'''
+first_element = my_list[0]  # Output is 10
+
+'''
+Accessing the second element of the list
+'''
+second_element = my_list[1] # Output is 20
+
+'''
+Accessing the last element of the list
+'''
+last_element = my_list[-1] # Output is 50
+
+'''
+Accessing the second_last element of the list
+'''
+
+second_last_element = my_list[-2] # Output is 40
+
+# NEGATIVE INDEXES ARE USED TO COUNT ELEMENTS FROM THE END OF THE LIST
+
+
+
+#=====================================================================
+
+#             COMMON OPERATIONS
+
+#=====================================================================
+
+
+'''
+Appending Elements
+
+'''
+
+my_list = [2,3,4,5,6]
+
+my_list.append(4) # it appendes element to the end of the list
+
+#Output is [2,3,4,5,6,4]
+
+
+'''
+Extending Arrays - used when you want to add multiple elements  to a list
+
+'''
+
+my_list = [2,3,4,5,6]
+
+my_list.extend(['a','b']) #output is [2,3,4,5,6,'a','b']
+
+my_list.extend([4,5, 6 ]) # output is [2,3,4,5,6,'a','b',4,5,6])
+
+'''
+Inserting Elements - Insert elements at specified positions in an array
+
+'''
+my_list = [1,4,6,7]
+my_list.insert(1,5)  # insert 5 at index 1
+# output is [1,4,6,7,4,5]  
+
+'''
+Deleting Elements - Removing elements from an array
+
+'''
+
+my_list = [1,4,6,7]
+list.remove(1) # remove element by value(removes one)
+list.pop(2) # remove element by index (removes element at index 2)
+del my_list[3]     # Delete element by index (delete element at index 3)
+
+#-----------------------------------------------------------------
+#              KEY DIFFERENCE BETWEEN pop() AND del()
+#----------------------------------------------------------------
+
+# pop() method not only removes element at index but it also returns
+# the removed element.
+# del() is a python statement used to delete elements
+#  in this context it remove an elemet at the index without
+# returning it.
+
+
+'''
+Slicing Elements - extacts a subset of elements from an existing list
+
+'''
+
+my_list = [1,2,3,4,5,6]
+
+sliced_list = my_list[1:4]
+
+'''
+Length of an Element - Find the number of elements in a list using the len() function.
+
+'''
+
+my_list = [1, 2, 3, 4]
+length = len(my_list)
+
+'''
+Checking for Existence of an Element in the list
+
+'''
+my_list = [1, 2, 3]
+exists = 3 in my_list
---
+++ week_one/day_one/readme.md
@@ -0,0 +1,55 @@
+# Day 1: Review Basic Data Structures
+
+## Arrays
+
+### Declaration and Initialization
+
+- Review how to declare and initialize arrays in your programming language of choice.
+
+### Accessing Elements
+
+- Refresh your knowledge of how to access elements in an array using indices (0-based indexing).
+
+### Common Operations
+
+- Practice appending elements to the end, inserting elements at specific positions, and deleting elements.
+
+### LeetCode Practice
+
+- Solve basic array-related problems on LeetCode to reinforce your understanding. Start with simple problems and gradually increase the complexity.
+
+## Linked Lists
+
+### Singly Linked Lists
+
+- Understand the structure of singly linked lists, including nodes and their next pointers.
+
+### Doubly Linked Lists
+
+- Review the structure of doubly linked lists, which have nodes with both forward and backward pointers.
+
+### Operations
+
+- Practice creating nodes, traversing the list, inserting nodes at different positions, and deleting nodes. Pay attention to edge cases.
+
+## Stacks and Queues
+
+### Stacks (LIFO)
+
+- Understand the Last-In-First-Out (LIFO) nature of stacks.
+- Implement a stack using an array or a linked list.
+- Implement stack operations like `push()` and `pop()`.
+
+### Queues (FIFO)
+
+- Understand the First-In-First-Out (FIFO) nature of queues.
+- Implement a queue using an array or a linked list.
+- Implement queue operations like `enqueue()` and `dequeue()`.
+
+### Use Cases
+
+- Refresh your understanding of when to use stacks and queues, and consider practical scenarios.
+
+### Practice
+
+- Create practical examples and solve problems that involve stacks and queues to reinforce your understanding.
